26527f54eb2c72a66531aa3d964643d3cebf8aef
txphu2302 <phu.tran232@hcmut.edu.vn>
2024-10-28 22:31:32 +0700

    Init for Ex6

--- STM32 Project/Core/Src/main.c
+++ STM32 Project/Core/Src/main.c
@@ -57,6 +57,22 @@ static void MX_GPIO_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+int timer0_counter = 0;
+int timer0_flag = 0;
+int TIMER_CYCLE = 10;
+void setTimer0 (int duration)
+{
+	timer0_counter = duration / TIMER_CYCLE;
+	timer0_flag = 0;
+}
+void timer_run()
+{
+	if(timer0_counter > 0)
+	{
+		timer0_counter--;
+		if(timer0_counter == 0) timer0_flag = 1;
+	}
+}
 
 /* USER CODE END 0 */
 
@@ -95,10 +111,16 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-
+  //TODO Ex6
+  setTimer0(1000);
   while (1)
   {
     /* USER CODE END WHILE */
+	  if(timer0_flag == 1)
+	  {
+		  HAL_GPIO_TogglePin(GPIOA, LED_RED_Pin);
+		  setTimer0 (2000);
+	  }
 
     /* USER CODE BEGIN 3 */
   }
@@ -240,6 +262,10 @@ static void MX_GPIO_Init(void)
 
 /* USER CODE BEGIN 4 */
 
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+	timer_run();
+}
 
 /* USER CODE END 4 */
 
